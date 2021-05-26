4b5093d7223a13666027ecbbf919e3d8ec4a1e56
Ivan Neverovsky <ivan@neverovsky.com.ua>
2021-05-26 15:47:50 +0300

    added project list templates render

--- src/app.ts
+++ src/app.ts
@@ -28,7 +28,6 @@ function validate(validatableInput: Validatable) {
   return isValid;
 }
 
-
 //Autobind decorator
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
@@ -40,6 +39,36 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     }
   }
   return adjDescriptor;
+}
+
+//ProjectList class
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: 'active' | 'finished') {
+    this.templateElement = document.getElementById('project-list')! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(this.templateElement.content, true);
+
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector('ul')!.id = listId;
+    this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS';
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('beforeend', this.element);
+  }
 }
 
 //ProjectInput Class
@@ -129,4 +158,6 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList('active');
+const finishedPrjList = new ProjectList('finished');
 
